e1d1a22c126ab26bbe90c1bfe2dac520624d6a4f
duda <Dudateic>
2026-06-27 22:32:48 -0300

    feat(profile): support avatar upload and static file serving

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,21 +1,12 @@
 import express from "express";
 import cors from "cors";
-import pool from "./config/database.js";
 import routes from "./routes/index.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
-app.get("/", async (req, res) => {
-    const result = await pool.query("SELECT NOW()");
-
-    res.json({
-        api: "CashBattle",
-        database: result.rows[0].now
-    });
-});
+app.use("/uploads", express.static("uploads"));
 
 app.use(routes);
 
